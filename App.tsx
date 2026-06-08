ab47875bc56b009e822728da0e2f0ff734bb1dc3
kvinitkarmkar <vinitkumarkarmkar836@gmail.com>
2026-06-08 15:37:02 +0530

    Refactor App component to display a simple greeting
    Removed unused imports and replaced the main return structure with a simple view displaying 'Hello'.

--- App.tsx
+++ App.tsx
@@ -1,25 +1,11 @@
 import React from 'react';
-import { StatusBar } from 'react-native';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { QueryClientProvider } from '@tanstack/react-query';
-import { queryClient } from './src/Lib/queryClient';
-import AppNavigator from './src/navigation/AppNavigator';
-
-// import {
-//   requestNotificationPermission,
-//   getFCMToken,
-//   setupNotificationListeners,
-// } from './src/utils/firebaseNotification';
+import { View, Text } from 'react-native';
 
 export default function App() {
-  // useEffect hata diya temporarily
-
   return (
-    <QueryClientProvider client={queryClient}>
-      <SafeAreaProvider>
-        <StatusBar barStyle="light-content" backgroundColor="#0F1923" />
-        <AppNavigator />
-      </SafeAreaProvider>
-    </QueryClientProvider>
+    <View style={{ flex: 1, backgroundColor: '#0F1923', 
+    alignItems: 'center', justifyContent: 'center' }}>
+      <Text style={{ color: 'white', fontSize: 24 }}>Hello</Text>
+    </View>
   );
 }
